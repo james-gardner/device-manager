9ff29f6b09d2f8e3391ca3b6d49ab1951af61dec
jagardne <ext-james.gardner@microsoft.com>
2014-12-04 10:40:31 +0000

    Added in dummy data

---
+++ data.js
@@ -0,0 +1,17 @@
+var devices = [
+	{ name : 'iphone 3g', status : 'in', type : 'phone'},
+	{ name : 'iphone 3gs', status : 'in', type : 'phone'},
+	{ name : 'iphone 4', status : 'in', type : 'phone'},
+	{ name : 'iphone 4s', status : 'in', type : 'phone'},
+	{ name : 'iphone 5', status : 'in', type : 'phone'},
+	{ name : 'iphone 5c', status : 'in', type : 'phone'},
+	{ name : 'iphone 5s', status : 'in', type : 'phone'},
+	{ name : 'iphone 6', status : 'in', type : 'phone'},
+	{ name : 'iphone 6+', status : 'in', type : 'phone'},
+	{ name : 'ipad 1', status : 'in', type : 'tablet'},
+	{ name : 'ipad 2', status : 'in', type : 'tablet'},
+	{ name : 'ipad 3', status : 'in', type : 'tablet'}
+	{ name : 'ipad Retina', status : 'in', type : 'tablet'}
+	{ name : 'ipad Mini', status : 'in', type : 'tablet'}
+	{ name : 'ipad Mini Retina', status : 'in', type : 'tablet'}
+]; 

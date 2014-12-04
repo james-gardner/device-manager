916c7ff40c198f71f19e2379bca9e505fa529fad
jagardne <ext-james.gardner@microsoft.com>
2014-12-04 11:24:26 +0000

    Added heaps more devices to dummy data

--- data.js
+++ data.js
@@ -1,17 +1,605 @@
-var devices = [
-	{ name : 'iphone 3g', status : 'in', type : 'phone'},
-	{ name : 'iphone 3gs', status : 'in', type : 'phone'},
-	{ name : 'iphone 4', status : 'in', type : 'phone'},
-	{ name : 'iphone 4s', status : 'in', type : 'phone'},
-	{ name : 'iphone 5', status : 'in', type : 'phone'},
-	{ name : 'iphone 5c', status : 'in', type : 'phone'},
-	{ name : 'iphone 5s', status : 'in', type : 'phone'},
-	{ name : 'iphone 6', status : 'in', type : 'phone'},
-	{ name : 'iphone 6+', status : 'in', type : 'phone'},
-	{ name : 'ipad 1', status : 'in', type : 'tablet'},
-	{ name : 'ipad 2', status : 'in', type : 'tablet'},
-	{ name : 'ipad 3', status : 'in', type : 'tablet'}
-	{ name : 'ipad Retina', status : 'in', type : 'tablet'}
-	{ name : 'ipad Mini', status : 'in', type : 'tablet'}
-	{ name : 'ipad Mini Retina', status : 'in', type : 'tablet'}
-]; 
+var devices = [{
+    "name": "Alcatel One Touch POP 7",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Alcatel One Touch Idol X",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Alcatel One Touch Fierce",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Alcatel One Touch Idol Mini",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Amazon Kindle Fire HDX",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Amazon Fire Phone",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Apple iPhone 5",
+    "status": "in",
+    "os": "iOS 6.0"
+}, {
+    "name": "Apple iPhone Mini",
+    "status": "in",
+    "os": "iOS 7.0"
+}, {
+    "name": "Apple iPhone 4S",
+    "status": "in",
+    "os": "iOS 7.0"
+}, {
+    "name": "Apple iPhone 5C",
+    "status": "in",
+    "os": "iOS 7.0"
+}, {
+    "name": "Apple iPad 4",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPad Air",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPad Mini",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple  iPad Mini 3",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPad Mini 2",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 4S",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 6 ",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 6 Plus ",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 6 Plus ",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 6 Plus ",
+    "status": "in",
+    "os": "iOS 8.0"
+}, {
+    "name": "Apple iPhone 4",
+    "status": "in",
+    "os": "iOS 5.1.1"
+}, {
+    "name": "Apple iPhone 3GS",
+    "status": "in",
+    "os": "iOS 6.1.3"
+}, {
+    "name": "Apple iPhone 4",
+    "status": "in",
+    "os": "iOS 6.1.3"
+}, {
+    "name": "Apple iPhone 4S",
+    "status": "in",
+    "os": "iOS 6.1.3"
+}, {
+    "name": "Apple iPod Touch 4",
+    "status": "in",
+    "os": "iOS 6.1.6"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 7.0.2"
+}, {
+    "name": "Apple iPad 4",
+    "status": "in",
+    "os": "iOS 7.0.4"
+}, {
+    "name": "Apple iPad Air",
+    "status": "in",
+    "os": "iOS 7.0.4"
+}, {
+    "name": "Apple iPhone 5",
+    "status": "in",
+    "os": "iOS 7.0.4"
+}, {
+    "name": "Apple iPad 2",
+    "status": "in",
+    "os": "iOS 7.1.1"
+}, {
+    "name": "Apple iPad Mini 2",
+    "status": "in",
+    "os": "iOS 7.1.1"
+}, {
+    "name": "Apple iPhone 4S",
+    "status": "in",
+    "os": "iOS 7.1.1"
+}, {
+    "name": "Apple iPhone 5C",
+    "status": "in",
+    "os": "iOS 7.1.1"
+}, {
+    "name": "Apple iPhone 5C",
+    "status": "in",
+    "os": "iOS 7.1.1"
+}, {
+    "name": "Apple iPhone 5C",
+    "status": "in",
+    "os": "iOS 7.1.1"
+}, {
+    "name": "Apple iPad 2",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Apple iPhone 5",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Apple iPhone 5C",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Apple iPhone 5C",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Apple iPhone 5S",
+    "status": "in",
+    "os": "iOS 7.1.2"
+}, {
+    "name": "Asus Padfone Infinity",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "BlackBerry 9300",
+    "status": "in",
+    "os": "BlackBerry 6.0"
+}, {
+    "name": "BlackBerry 9810",
+    "status": "in",
+    "os": "BlackBerry 6.1"
+}, {
+    "name": "BlackBerry 9360",
+    "status": "in",
+    "os": "BlackBerry 7.0"
+}, {
+    "name": "BlackBerry 9900",
+    "status": "in",
+    "os": "BlackBerry 7.0"
+}, {
+    "name": "Google  Nexus 9 ",
+    "status": "in",
+    "os": "Android 5.0 L"
+}, {
+    "name": "Google Nexus 7",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Google Nexus 10",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Google Nexus 4",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Google Nexus 7 2013",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Google Nexus 5",
+    "status": "in",
+    "os": "Android 4.4.4"
+}, {
+    "name": "HTC One Max",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "HTC Windows Phone 8X",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "HTC Golf",
+    "status": "in",
+    "os": "Android 4.0.3"
+}, {
+    "name": "HTC One S",
+    "status": "in",
+    "os": "Android 4.0.3"
+}, {
+    "name": "HTC One X",
+    "status": "in",
+    "os": "Android 4.0.3"
+}, {
+    "name": "HTC Sensation",
+    "status": "in",
+    "os": "Android 4.0.3"
+}, {
+    "name": "HTC One S",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "HTC Thunderbolt",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "HTC First",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "HTC Desire 601",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "HTC One XL",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "HTC DROID DNA",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "HTC One M8",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "HTC One M8",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "HTC One Mini",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Huawei Ascend P1",
+    "status": "in",
+    "os": "Android 4.0.3"
+}, {
+    "name": "Huawei Ascend Y300",
+    "status": "in",
+    "os": "Android 4.1.1"
+}, {
+    "name": "LG Optimus F3 P659",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "LG Optimus L5 E617G",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "LG G2",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "LG G2 Mini",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "LG G3",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "LG G-Flex",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Motorola DROID Ultra",
+    "status": "in",
+    "os": "Android 4.4"
+}, {
+    "name": "Motorola DROID Razr",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Motorola DROID Razr M",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Motorola Moto E",
+    "status": "in",
+    "os": "Android 4.4.3"
+}, {
+    "name": "Motorola DROID Mini",
+    "status": "in",
+    "os": "Android 4.4.4"
+}, {
+    "name": "Motorola Moto G ",
+    "status": "in",
+    "os": "Android 4.4.4"
+}, {
+    "name": "Nokia Lumia 620",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Nokia Lumia 920",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Nokia Lumia 920",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Nokia Lumia 920",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Nokia Lumia 920",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Nokia Lumia 928",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Nokia Lumia 928",
+    "status": "in",
+    "os": "Windows Phone"
+}, {
+    "name": "Samsung Galaxy Nexus",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Samsung Galaxy Note III",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Samsung Galaxy S III",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Samsung Galaxy S III",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Samsung Galaxy S III LTE",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Samsung Galaxy S II (I9100)",
+    "status": "in",
+    "os": "Android 2.3.3"
+}, {
+    "name": "Samsung Galaxy Xcover (GT-S5690)",
+    "status": "in",
+    "os": "Android 2.3.6"
+}, {
+    "name": "Samsung Galaxy Rugby LTE",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Samsung Galaxy Note (N7000)",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Samsung Galaxy Tab 10.1 ICS (P7500)",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Samsung S730M",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Samsung Galaxy Note II (I317)",
+    "status": "in",
+    "os": "Android 4.1.1"
+}, {
+    "name": "Samsung Galaxy Express I8730",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Samsung Galaxy Note 10.1 GT-N8013",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Samsung Galaxy Note 10.1 GT-N8013",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Samsung I8190-Galaxy S III Mini (I317)",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Samsung Nexus S Digital",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Samsung Galaxy Ace III",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Express 2",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Grand I9082",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy S IV Mini",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Tab 3 Lite-7.0",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Tab 3 1.0-P5210",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy S IV 4.2 (I337)",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy S4 Mini (I9190)",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy S4 Mini (I9190)",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Note 8.0 (N5110)",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Mega 6.3 (SGH-I527M)",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung SGH-I537 4.2",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Samsung Galaxy Note III N900A 4.4",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy Note III Neo",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy Note Pro 12.2",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy Note Pro 12.2",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S5",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S5 SM-G900A",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S5 Sport",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy Tab 4 8\" ",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy Tab S 10.5 T800",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S IV 4.4 (GT-I9505)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S IV 4.4 (I337)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S IV 4.4 (I337)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S IV 4.4 (I337)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S IV 4.4 (I337)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S III 4.4 (I337)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S III 4.4 (I337)",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Samsung Galaxy S5",
+    "status": "in",
+    "os": "Android 4.4.4"
+}, {
+    "name": "Sony Xperia E1",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Sony Xperia M",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Sony Xperia Z",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Sony Xperia Z Ultra",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Sony Xperia ZL",
+    "status": "in",
+    "os": "Android 4.3"
+}, {
+    "name": "Sony Xperia Miro",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Sony Xperia S",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Sony Xperia T",
+    "status": "in",
+    "os": "Android 4.0.4"
+}, {
+    "name": "Sony Xperia SP",
+    "status": "in",
+    "os": "Android 4.1.2"
+}, {
+    "name": "Sonynn Xperia Z1",
+    "status": "in",
+    "os": "Android 4.2.2"
+}, {
+    "name": "Sonynn Xperia Tablet Z2",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Sonynn Xperia Z2",
+    "status": "in",
+    "os": "Android 4.4.2"
+}, {
+    "name": "Sonynn Xperia Z1",
+    "status": "in",
+    "os": "Android 4.4.4"
+}]''

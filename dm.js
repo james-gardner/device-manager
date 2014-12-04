ce5088986414c0d0470b9aa3b78e6794479a6271
jagardne <ext-james.gardner@microsoft.com>
2014-12-04 15:07:52 +0000

    simplified event selection

--- dm.js
+++ dm.js
@@ -4,16 +4,28 @@
   var DeviceManager = function(el, data) {
     this.el = el;
     this.data = data;
+
+    this.el.addEventListener('click', function (e) {
+      if(e.target.className === 'device') {
+        this._onDeviceSelected(e.target.getAttribute('data-id'));
+      }
+    }.bind(this));
   };
 
   var p = DeviceManager.prototype;
 
+  p._onDeviceSelected = function (id) {
+    console.log(this.data[id]);
+  };
+
   p.render = function() {
     var li, el = this.el;
 
-    this.data.forEach(function(obj) {
+    this.el.innerHTML = null;
+
+    this.data.forEach(function(obj, index) {
       li = document.createElement('li');
-      li.innerHTML = '<p><strong>' + obj.name + '</strong></p>';
+      li.innerHTML = '<p class="device" data-id="' + index + '">' + obj.name + '</p>';
 
       el.appendChild(li);
     });
